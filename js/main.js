4075f601bfed02906c2c9968a0de92597ca6472e
Byun sumi <selina2000@naver.com>
2021-08-03 02:59:19 +0900

    starbucks header+visual+notice+rewards

--- js/main.js
+++ js/main.js
@@ -2,14 +2,110 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 console.log(":JS");
-searchEl.addEventListener('click', function() {
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
-searchInputEl.addEventListener('focus',function() {
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
-searchInputEl.addEventListener('blur',function() {
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
+  searchInputEl.setAttribute('placeholder', '');
 });
+
+// BADGE
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  // console.log(window.scrollY); 
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // badgeEl.style.display = 'none';
+    // gsap.to(애니매이션 처리할 요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+
+  // https://www.google.com/search?q=gsap+cdn&oq=gsap&aqs=chrome.1.69i57j0i20i263i512j0i512l8.5422j0j4&sourceid=chrome&ie=UTF-8
+  else {
+    // 배지 나오기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+
+    })
+  }
+}, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(애니매이션 처리할 요소, 지속시간(초), 옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  });
+});
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
+  },
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백 
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+  }
+});
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHiddePromotion = false;
+
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHiddePromotion = !isHiddePromotion;
+  if (isHiddePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+})
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, //선택자
+    random(1.5, 2.5),   //애니매이션 동작시간
+    { //옵션
+    y: size,
+    repeat: -1, //반복 -1이면 무한
+    yoyo: true, //한번 재생된 애니매이션을 반대로 재생
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  })
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
