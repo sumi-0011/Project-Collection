8a5fade308af8470e09f9edfca49bb796746bc2b
sumi-0011 <selina2000@naver.com>
2021-08-12 00:37:42 +0900

    reserve_magazin + store + footer animation plus

--- js/main.js
+++ js/main.js
@@ -1,37 +1,50 @@
 // gsap
 const visual_set_commonEl = document.querySelectorAll('.set__common');
-
+const scroll_leftEls = document.querySelectorAll('.scroll-left');
+const scroll_rightEls = document.querySelectorAll('.scroll-right');
 visual_set_commonEl.forEach(function(elem, index) {
   gsap.to(elem,{
     delay: (index+1) *0.7,
     opacity:1
   })
 })
+scroll_leftEls.forEach(function(elem) {
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".scroll-left", {
-  scrollTrigger: {
-    trigger: ".scroll-left",
-    start: "top 80%",
-    scrub: 5,
-    end: 100
-  },
-  x: 1000, 
-  opacity:1
-
-});
-gsap.to(".scroll-right", {
-  scrollTrigger: {
-    trigger: ".scroll-right",
-    // markers: true,
-    start: "top 80%",
-    scrub: 5,
-    end: 100
-  },
-  x: -1000, 
-  opacity:1
+  gsap.to(elem, {
+    scrollTrigger: {
+      trigger: elem,
+      // markers: true,
+      start: "top 80%",
+      scrub: 5,
+      end: "+=50px"
+    },
+    x: 1000, 
+    opacity:1
   
-});
+  });
+})
+gsap.registerPlugin(ScrollTrigger);
+
+scroll_rightEls.forEach(function(elem) {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(elem, {
+    scrollTrigger: {
+      trigger: elem,
+      // markers: true,
+      start: "top 80%",
+      scrub: 5,
+      end: "+=50px"
+
+    },
+    x: -1000, 
+    opacity:1
+    
+  });
+})
+
+
 gsap.to(".scroll-opacity", {
   scrollTrigger: {
     trigger: ".scroll-opacity",
