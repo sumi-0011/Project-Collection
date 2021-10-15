2a936d915f41ff1e3c86c3544500581b40061200
sumi.byun <selina2000@naver.com>
2021-10-16 01:54:46 +0900

    react-native code modify

---
+++ client/src/component/Recycle_Info_Detail_Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Recycle_Info_Detail_Screen = () => {
+    return (
+        <div className="screen" >
+            <p className="screen-text">
+                This is Recycle Info Detail Screen
+            </p>
+        </div>
+    );
+};
+
+
+
+export default Recycle_Info_Detail_Screen;
