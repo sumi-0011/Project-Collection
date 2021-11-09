8b772b7d059c0a4ba3ffe821bc605c1684eb4f17
sumi.byun <selina2000@naver.com>
2021-11-09 22:36:15 +0900

    주석 삭제 + classNmae 수정

--- src/page/Header.js
+++ src/page/Header.js
@@ -4,85 +4,43 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     // <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light shadow">
-      <div class="container d-flex justify-content-between align-items-center">
+    <nav className="navbar navbar-expand-lg navbar-light shadow">
+      <div className="container d-flex justify-content-between align-items-center">
         <a
-          class="navbar-brand text-success logo h1 align-self-center"
+          className="navbar-brand text-success logo h1 align-self-center"
           href="index.html"
         >
           Happy Cycle
         </a>
-
-        {/* <button
-          class="navbar-toggler border-0"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#templatemo_main_nav"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button> */}
-
         <div
-          class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between"
+          className="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between"
           id="templatemo_main_nav"
         >
-          <div class="flex-fill">
-            <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-              <li class="nav-item">
+          <div className="flex-fill">
+            <ul className="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+              <li className="nav-item">
                 <Link to="/">
-                  <span class="nav-link">사물인식</span>
+                  <span className="nav-link">사물인식</span>
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/map">
-                  <span class="nav-link">배출 장소 확인</span>
+                  <span className="nav-link">배출 장소 확인</span>
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/info">
-                  <span class="nav-link">배출방법</span>
+                  <span className="nav-link">배출방법</span>
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/user">
-                  <span class="nav-link">마이페이지</span>
+                  <span className="nav-link">마이페이지</span>
                 </Link>
               </li>
             </ul>
           </div>
-          {/* <div class="navbar align-self-center d-flex">
-            <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
-              <div class="input-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="inputMobileSearch"
-                  placeholder="Search ..."
-                />
-                <div class="input-group-text" />
-                <i class="fa fa-fw fa-search"></i>
-              </div>
-            </div>
-          </div> */}
-          {/* <a
-            class="nav-icon d-none d-lg-inline"
-            href="#"
-            data-bs-toggle="modal"
-            data-bs-target="#templatemo_search"
-          >
-            <i class="fa fa-fw fa-search text-dark mr-2"></i>
-          </a> */}
-          {/* <a class="nav-icon position-relative text-decoration-none" href="#">
-                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
-                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span>
-                    </a> */}
-          {/* <a class="nav-icon position-relative text-decoration-none" href="#">
-                        <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span>
-                    </a> */}
+         
         </div>
       </div>
     </nav>
