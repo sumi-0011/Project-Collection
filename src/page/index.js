e2a152cb754d0a2246d8e12bc3b3422ff8cd8065
sumi.byun <selina2000@naver.com>
2021-11-09 17:23:54 +0900

    arrange

--- src/page/index.js
+++ src/page/index.js
@@ -2,3 +2,6 @@ export { default as Image } from './image_page';
 export { default as Map } from './map_page';
 export { default as User } from './user_page';
 export { default as Info } from './info_page';
+export { default as Header } from './Header';
+export { default as Modal } from './Modal';
+export { default as Main } from './Main';
