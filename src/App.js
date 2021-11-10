396ec02e8ba3d367a3e905f3dcdc04d0c319c16f
sumi.byun <selina2000@naver.com>
2021-11-10 15:29:21 +0900

    test용 코드 분리

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
-import PostList from "./components/PostList.js";
-import Detail from "./components/Detail.js";
+import PostList from "./components/Test/PostList.js";
+import Detail from "./components/Test/Detail.js";
 
 function App() {
   return (
     <BrowserRouter>
      <TestNav/>
-      <Switch>
-        <Route exact path="/main" component={PostList} />
-        {/* 이건 나중에 바꾸도록? 숫자로? */}
-        <Route exact path="/main/posts/:id" component={Detail} />
-        
-      </Switch>
+     <TestRouter/>
     </BrowserRouter>
   );
 }
@@ -21,13 +16,22 @@ function TestNav() {
   return (
     <div>
       <Link to="/main">
-        <button className="nav-link">게시글 리스트 화면</button>
+        <button className="nav-link">리스트 </button>
       </Link>
       <Link to="/main/posts/1">
-        <button className="nav-link">게시글 1 </button>
+        <button className="nav-link"> 1 </button>
       </Link>
     </div>
   );
 }
-
+function TestRouter() {
+  return(
+    <Switch>
+    <Route exact path="/main" component={PostList} />
+    {/* 이건 나중에 바꾸도록? 숫자로? */}
+    <Route exact path="/main/posts/:id" component={Detail} />
+    
+  </Switch>
+  )
+}
 export default App;
--- src/components/Test/PostList.js
+++ src/components/Test/PostList.js
@@ -44,3 +44,4 @@ function PostList() {
 }
 
 export default PostList;
+ 
