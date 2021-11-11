60a6ff03cdd8c76dec1791b6a8b0f8746a17d67a
fault2000 <gkxodnrcjswo@o.cnu.ac.kr>
2021-11-11 14:11:29 +0900

    지도병합

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as ConfirmeState } from './ConfirmeState';
 export { default as Main } from './Main';
-export { default as Map } from './Map';
+export { default as Map } from './MapView';
 export { default as SocialDistance } from './SocialDistance'; 
 export { default as NoSiderBarPage } from './NoSiderBarPage'; 
 
