43b8c18287f5294cdada9eec53f76a0d11b0cadf
sumi.byun <selina2000@naver.com>
2021-11-14 20:50:00 +0900

    auto slide

--- js/swiper.js
+++ js/swiper.js
@@ -6,10 +6,10 @@
         loop: true,
         loopAdditionalSlides: 1,
         pagination: false, // pager 여부
-        // autoplay: {  // 자동 슬라이드 설정 , 비 활성화 시 false
-        //     delay: 3000,   // 시간 설정
-        //     disableOnInteraction: false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
-        // },
+        autoplay: {  // 자동 슬라이드 설정 , 비 활성화 시 false
+            delay: 3000,   // 시간 설정
+            disableOnInteraction: false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+        },
         navigation: {   // 버튼 사용자 지정
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
