5eb37fd01a5ab4fcf8a387032b46fe1ed614b171
sumi.byun <selina2000@naver.com>
2021-11-18 03:26:37 +0900

    그래프 기능 추가 + recharts 패키지 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 // import
 function Main() {
   return (
@@ -9,6 +18,24 @@ function Main() {
 }
 
 function Tempelate() {
+
+  const list = [
+    {
+      name: "Page A",
+      "DECIDE_CNT": 10,
+      
+    },
+    {
+      name: "Page B",
+      "DECIDE_CNT": 5,
+      
+    },
+    {
+      name: "Page C",
+      "DECIDE_CNT": 25,
+      
+    },
+  ]
   return (
     <div id="page-wrapper">
       {/* <MainContainer /> */}
@@ -43,7 +70,59 @@ function Tempelate() {
           </div>
         </div>
       </div>
+      <Graph list={list}/>
     </div>
+  );
+}
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  
+];
+
+function Graph({list}) {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={list}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="DECIDE_CNT"
+        stroke="#8884d8"
+        // activeDot={{ r: 8 }}
+      />
+      {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+    </LineChart>
   );
 }
 
