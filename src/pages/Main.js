033b535928a52eb01e80fa4b85ebd4a2414e0d39
sumi.byun <selina2000@naver.com>
2021-11-18 03:02:26 +0900

    확진자 현황 틀

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,9 +12,39 @@ function Tempelate() {
   return (
     <div id="page-wrapper">
       {/* <MainContainer /> */}
+      <div className="ConfirmedState-container">
+        <div className="setion-containter">
+          <div className="sortation">
+            <div className="CS__title">구분</div>
+            <div className="CS__content">일일</div>
+          </div>
+
+          <div className="item">
+            <div className="item__title">확진자수</div>
+            <div className="item__content">11111</div>
+          </div>
+          <div className="item">
+            <div className="item__title">치료중 환자수</div>
+            <div className="item__content">11111</div>
+          </div>
+          <div className="item">
+            <div className="item__title">사망자수</div>
+            <div className="item__content">11111</div>
+          </div>
+        </div>
+        <div className="setion-containter">
+          <div className="item">
+            <div className="item__title">누적 확진자수</div>
+            <div className="item__content">11111</div>
+          </div>
+          <div className="item">
+            <div className="item__title">누적 사망자수</div>
+            <div className="item__content">11111</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default Main;
