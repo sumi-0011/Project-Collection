e2ed8f01d4b22253318f2c392321bfadde619ed2
fault2000 <gkxodnrcjswo@o.cnu.ac.kr>
2021-11-18 00:40:46 +0900

    지도 크기 변경

--- src/pages/MapView.js
+++ src/pages/MapView.js
@@ -216,7 +216,7 @@ export default function MapView(){
     
     return (
         <div>
-            <div id="map" style={{width:"100%", height:"100vh", position:"relative", overflow:"hidden"}}></div>
+            <div id="map" style={{width:"50%", height:"50vh", position:"relative", overflow:"hidden"}}></div>
 
             <div id="menu_wrap">
                 <div>
