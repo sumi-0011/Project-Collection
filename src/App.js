c3125a8040e40a82ba342d8ba2ede3b6b45d68da
sumi.byun <selina2000@naver.com>
2021-11-26 15:31:44 +0900

    firebase add Test 진행

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Main, NoSiderBarPage,SocialDistance,ConfirmeState,MapView } from "./pages/index";
 import { Header, Footer } from "./components/index";
 import "./assets/css/main.css";
-import {authService} from './firebase';
+import {authService, dbService} from './firebase';
 function App() {
  
   console.log(authService.currentUser);
@@ -14,12 +14,47 @@ function App() {
       {/* <a href='html\index.html'>sss</a> */}
       <Header />
       <RouterList />
+      <TestD/>
       {/* <TestNav/>
      <TestRouter/> */}
 
       {/* <Footer/> */}
     </BrowserRouter>
   );
+}
+function TestD() {
+  // #3.1 Nweeting! (06:37) 어떻게 가져올지는 이 다음에
+  const [nweet,setNweet] = useState("sumi");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+
+  }
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+    <form onSubmit={onSubmit}>
+      <input
+        value={nweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Nweet" />
+    </form>
+  </div>
+  )
 }
 function RouterList() {
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-
+import "firebase/firestore";
 const firebaseConfig = {
-    //추가할곳
+
 };
 firebase.initializeApp(firebaseConfig);
-export default firebase.initializeApp(firebaseConfig);
+export const firebaseInstance = firebase;
+export const dbService = firebase.firestore();
 export  const authService = firebase.auth();
+
+export default firebase;
