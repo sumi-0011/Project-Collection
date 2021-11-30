9318530efb9f6a084b10b7e5750d79297f930cdd
sumi.byun <selina2000@naver.com>
2021-12-01 01:27:09 +0900

    error report 페이지 form

--- src/pages/ErrorReport.js
+++ src/pages/ErrorReport.js
@@ -1,13 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import '../css/ErrorReport.css';
 function ErrorReport() {
-    return (
-        <div id="main-wrapper">
-      <div className="container">
-        <h1>Welcome, ErrorReport</h1>
+  return (
+    <div id="main-wrapper">
+      <div id="error-report-container">
+        <div className="container">
+          {/* <h1>ErrorReport</h1> */}
+          <form>
+            <div className="form-group">
+              <label htmlFor="inputReportTitle">title</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="report title input"
+                id="inputReportTitle"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputReportContent">report</label>
+              <textarea
+                className="form-control"
+                id="inputReportContent"
+                rows="3"
+                placeholder="report error input"
+              ></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-    )
+  );
 }
 
-export default ErrorReport
+export default ErrorReport;
