db94d11fbc0593aec2c0de633d9c8619ef7e337d
sumi.byun <selina2000@naver.com>
2022-03-15 00:01:45 +0900

    post 정리

--- express.js
+++ express.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+var bodyParser = require("body-parser");
 
+app.set("view engine", "jade");
+app.set("views", "./views");
 app.use(express.static("public")); //public 이라는 폴더를 정적인 파일이 존재하는 폴더로 하겠다.
+app.use(bodyParser.urlencoded({ extension: false }));
+app.get("/form_receiver", (req, res) => {
+  let title = req.query.title;
+  let description = req.query.description;
 
+  res.send(title + ", " + description);
+});
+app.post("/form_receiver", (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+
+  res.send(title + ", " + description);
+});
+app.get("/form", function (req, res) {
+  res.render("form");
+});
 app.get("/topic/:id", function (req, res) {
   let topics = ["JavaScript is...", "NodeJs is...", "Express is..."];
   let output = `<a href="/topic?id=0">JavaScript</a><br><a href="/topic?id=1">NodeJs</a><br><a href="/topic?id=2">Express</a><br>${
